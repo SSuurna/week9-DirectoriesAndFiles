f2a87ecdb8a81f382576bde505808095707c3fa7
Sander Suurna <sande@DESKTOP-8IN12K6>
2021-11-22 15:35:54 +0200

    New project

---
+++ DirectoriesAndFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoriesAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectoy = @"C:\Users\SS\Samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+            if (Directory.Exists($"{rootDirectoy}\\{newDirectory}") && File.Exists($"{rootDirectoy}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and file exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectoy}\\{newDirectory}");
+                File.Create($"{rootDirectoy}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
---
+++ ShoppingList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            string rootDirectoy = @"C:\Users\SS\Samples";
+            string newDirectory = @"ShoppingList";
+            string fileName = @"myShoppingList.txt";
+            Directory.CreateDirectory($"{rootDirectoy}\\{newDirectory}");
+
+            if (Directory.Exists($"{rootDirectoy}\\{newDirectory}") && File.Exists($"{rootDirectoy}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and file exist.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectoy}\\{newDirectory}");
+                File.Create($"{rootDirectoy}\\{newDirectory}\\{fileName}");
+            }
+
+            Console.Clear();
+
+            string fileLocation = @"C:\Users\SS\Samples\ShoppingList";
+            string FileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{FileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myShoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine($"Your wish: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{FileName}", myShoppingList);
+        }
+    }
+}
